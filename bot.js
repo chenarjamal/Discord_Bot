a2aa72e0aab7698655280c78a29242ac2b418438
navaneeth <justmailme2209@gmail.com>
2020-11-25 17:51:04 +0530

    First
    Completed sucessfully!

---
+++ bot.js
@@ -0,0 +1,38 @@
+const {Client}=require('discord.js')
+const {play,stop} =require('./commands')
+const bot=new Client();
+bot.login(process.env.token)
+bot.on('ready',()=>{
+   
+    console.log("BOT IS UP AND RUNNING")})
+
+bot.on('message',(msg)=>{
+    if(msg.author.bot)return
+ const prefix="#"
+ if(!msg.content.startsWith(prefix))return
+const commandName=getCommandName(prefix,msg.content)
+const args=getCommandArgs(prefix,msg.content)
+
+if(commandName==='podu'){
+    return play(msg,args)}
+
+  if(commandName==='kelambu'){
+    return stop(msg,args)
+  }
+
+
+})
+
+function getCommandName(prefix,content){
+    return content
+    .slice(prefix.length)
+    .split(' ')[0]
+}
+
+
+function getCommandArgs(prefix,content){
+    return content
+    .slice(prefix.length)
+    .split(' ')
+    .slice(1)
+}
---
+++ commands.js
@@ -0,0 +1,35 @@
+const { search } = require('ffmpeg-static')
+const downloadYT=require('ytdl-core')
+const searchYT=require('yt-search')
+
+async function play(msg,...args){
+const vc=msg.member.voice.channel
+
+const connection=await vc.join()
+const video=await findVideo(args.join(' '))
+
+if(video){
+    const stream=downloadYT(video.url,{filter:'audioonly'})
+    connection.play(stream,{seek:0,volume:10})
+
+    await msg.reply(`Neengal ketu kondu irupathu     ${video.title}`)
+}else{
+    await msg.reply(`Error!Serithu neram after try seyavum!`)
+}
+
+}
+async function findVideo(query){
+    const result=await searchYT(query)
+    return (result.videos.length>1)?result.videos[0]:null
+}
+
+async function stop(msg){
+    const vc=msg.member.voice.channel
+    await vc.leave()
+
+    await msg.reply("Stopped!")
+}
+
+
+module.exports.play=play
+module.exports.stop=stop
